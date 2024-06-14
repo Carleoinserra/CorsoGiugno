01d068bb35fa64aa67bfcd74f7606772bf2cb5c4
Carleoinserra <inserracarlo@gmail.com>
2024-06-14 12:14:51 +0200

    Update carta.php

--- carta.php
+++ carta.php
@@ -6,15 +6,19 @@ $randomNum = (rand(1, 4));
 echo("Benvenuto " . $nome . "<br>");
 if ($randomNum == 1){
     echo ("E' uscito Cuori");
+    echo ("<img src = 'cuori.jpeg'>");
 }
 else if ($randomNum == 2){
     echo ("E' uscito Fiori");
+    echo ("<img src = 'fiori.jpeg'>");
 }
 else if ($randomNum == 3){
     echo ("E' uscito Picche");
+    echo ("<img src = 'picche.jpeg'>");
 }
 else if ($randomNum == 4){
     echo ("E' uscito Quadri");
+    echo ("<img src = 'quadri.jpeg'>");
 }
 
 echo("<hr>");
